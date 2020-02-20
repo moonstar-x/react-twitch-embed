de5022047e665ded05897baa11f253bb73aa11dc
moonstar-x <christian.lopez99@outlook.com>
2020-02-20 13:24:58 -0500

    Created basic TwitchEmbed following Twitch API.

--- example/src/App.js
+++ example/src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import TwitchEmbed from 'react-twitch-embed';
 
+const channels = ['loltyler1', 'moonstar_x', 'method', 'Rainbow6'];
+
+const logArguments = (...args) => {
+  console.log(args);
+};
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      enabled: true,
+      channel: 'loltyler1'
+    };
+  }
+
   render() {
+    const { channel, enabled } = this.state;
+
     return (
       <div>
-        <TwitchEmbed text="hello" />
+        <button onClick={() => this.setState({ enabled: !this.state.enabled})}>Enable/Disable</button>
+        <button onClick={() => this.setState({ channel: channels[Math.floor(Math.random() * channels.length)] })}>Change channel</button>
+        {
+          enabled &&
+            <TwitchEmbed channel={channel} onAuthenticate={logArguments} onVideoPlay={logArguments} onVideoReady={logArguments} onVideoPause={logArguments} />
+        }
       </div>
     );
   }
--- src/components/TwitchEmbed.js
+++ src/components/TwitchEmbed.js
@@ -1,16 +1,102 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import isRequiredIf from 'react-proptype-conditional-require';
+import { TWITCH_EMBED_URL } from '../constants';
 
-const TwitchEmbed = ({ text }) => {
-  return (
-    <div>
-      {text}
-    </div>
-  );
-};
+class TwitchEmbed extends Component {
+  componentDidMount() {
+    if (window.Twitch && window.Twitch.Embed) {
+      return this._createEmbed();
+    }
+
+    const scriptElement = document.createElement('script');
+    scriptElement.setAttribute('src', TWITCH_EMBED_URL);
+
+    scriptElement.addEventListener('load', () => {
+      this._createEmbed();
+    });
+
+    document.body.appendChild(scriptElement);
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (
+      prevProps.channel !== this.props.channel ||
+      prevProps.width !== this.props.width ||
+      prevProps.height !== this.props.height
+    ) {
+      this.embed = null;
+      document.getElementById(prevProps.id).innerHTML = '';
+      this._createEmbed();
+    }
+  }
+
+  _createEmbed() {
+    this.embed = new window.Twitch.Embed(this.props.id, {
+      allowfullscreen: this.props.allowFullscreen,
+      channel: this.props.channel,
+      collection: this.props.collection,
+      'font-size': this.props['font-size'],
+      height: this.props.height,
+      layout: this.props.layout,
+      theme: this.props.theme,
+      video: this.props.video,
+      width: this.props.width
+    });
+
+    this._addEventListeners();
+  }
+
+  _addEventListeners() {
+    this.embed.addEventListener(window.Twitch.Embed.AUTHENTICATE, this.props.onAuthenticate);
+    this.embed.addEventListener(window.Twitch.Embed.VIDEO_PLAY, this.props.onVideoPlay);
+    this.embed.addEventListener(window.Twitch.Embed.VIDEO_PAUSE, this.props.onVideoPause);
+    this.embed.addEventListener(window.Twitch.Embed.VIDEO_READY, this.props.onVideoReady);
+  }
+
+  render() {
+    const { id, width, height } = this.props;
+
+    return (
+      <div style={{ width, height }} id={id} />
+    );
+  }
+}
+
+TwitchEmbed.COLLECTION_VIDEO_REQUIRED_MESSAGE = 'A video prop must be provided when the collection prop is specified.';
 
 TwitchEmbed.propTypes = {
-  text: PropTypes.string.isRequired
+  id: PropTypes.string,
+  allowFullscreen: PropTypes.bool,
+  channel: PropTypes.string,
+  collection: PropTypes.string,
+  'font-size': PropTypes.oneOf(['small', 'medium', 'large']),
+  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  layout: PropTypes.oneOf(['video-with-chat', 'video']),
+  theme: PropTypes.oneOf(['light', 'dark']),
+  video: isRequiredIf(PropTypes.string, (props) => !!props.collection, TwitchEmbed.COLLECTION_VIDEO_REQUIRED_MESSAGE),
+  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  onAuthenticate: PropTypes.func,
+  onVideoPlay: PropTypes.func,
+  onVideoPause: PropTypes.func,
+  onVideoReady: PropTypes.func
+};
+
+TwitchEmbed.defaultProps = {
+  id: 'twitch-embed',
+  allowFullscreen: true,
+  channel: null,
+  collection: null,
+  'font-size': 'small',
+  height: 480,
+  layout: 'video-with-chat',
+  theme: 'light',
+  video: null,
+  width: 940,
+  onAuthenticate: () => null,
+  onVideoPlay: () => null,
+  onVideoPause: () => null,
+  onVideoReady: () => null
 };
 
 export default TwitchEmbed;
