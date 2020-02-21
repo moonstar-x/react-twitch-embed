2421d395d33c0d5512fcba4fbc4d34460811e53f
moonstar-x <christian.lopez99@outlook.com>
2020-02-21 11:38:28 -0500

    Removed default export.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import TwitchEmbed from './components/TwitchEmbed';
 import TwitchChat from './components/TwitchChat';
 import TwitchClip from './components/TwitchClip';
 
-export default TwitchEmbed;
-
 export {
+  TwitchEmbed,
   TwitchChat,
   TwitchClip
 };
