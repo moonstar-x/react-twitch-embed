594a5d1674ed72b2d913b485ae834d5ec3d35890
moonstar-x <christian.lopez99@outlook.com>
2020-02-20 21:19:47 -0500

    Cleaned up example file and removed conditional require proptype package.

--- example/src/App.js
+++ example/src/App.js
@@ -3,10 +3,6 @@ import TwitchEmbed from 'react-twitch-embed';
 
 const channels = ['loltyler1', 'moonstar_x', 'method', 'Rainbow6'];
 
-const logArguments = (...args) => {
-  console.log(args);
-};
-
 class App extends Component {
   constructor(props) {
     super(props);
@@ -17,6 +13,21 @@ class App extends Component {
       video: '285064366',
       collection: 'YfGvvNZI9RWlEQ'
     };
+
+    this.handleEnable = this.handleEnable.bind(this);
+    this.handleChangeChannel = this.handleChangeChannel.bind(this);
+  }
+
+  handleEnable() {
+    this.setState({
+      enabled: !this.state.enabled
+    });
+  }
+
+  handleChangeChannel() {
+    this.setState({
+      channel: channels[Math.floor(Math.random() * channels.length)]
+    });
   }
 
   render() {
@@ -24,17 +35,12 @@ class App extends Component {
 
     return (
       <div>
-        <button onClick={() => this.setState({ enabled: !this.state.enabled})}>Enable/Disable</button>
-        <button onClick={() => this.setState({ channel: channels[Math.floor(Math.random() * channels.length)] })}>Change channel</button>
+        <button onClick={this.handleEnable}>Enable/Disable</button>
+        <button onClick={this.handleChangeChannel}>Change channel</button>
         {
           enabled &&
             <TwitchEmbed video={video} collection={collection} channel={channel} withChat={false} />
         }
-        <iframe src="https://player.twitch.tv/?autoplay=false&collection=YfGvvNZI9RWlEQ" frameBorder="0"
-                allowFullScreen="true" scrolling="no" height="378" width="620"></iframe>
-        <a href="https://www.twitch.tv/collections/YfGvvNZI9RWlEQ?tt_content=text_link&tt_medium=vod_embed"
-           >Watch
-          Test collection from moonstar_x on www.twitch.tv</a>
       </div>
     );
   }
