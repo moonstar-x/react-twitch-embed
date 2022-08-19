ba2d9bac12f63aaa77e5455846ba09d3a47316c0
moonstar-x <christian.lopez99@outlook.com>
2022-08-19 14:10:59 -0500

    Created a useScript hook.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import TwitchChat, { TwitchChatProps } from './components/TwitchChat';
 import TwitchClip, { TwitchClipProps } from './components/TwitchClip';
+import useScript, { ScriptState } from './hooks/useScript';
 
 export {
   TwitchChat,
   TwitchChatProps,
   TwitchClip,
-  TwitchClipProps
+  TwitchClipProps,
+
+  useScript,
+  ScriptState
 };
