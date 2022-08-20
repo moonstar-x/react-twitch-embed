bf7f83a4fc13b633944158a2442baa7a59e18641
moonstar-x <christian.lopez99@outlook.com>
2022-08-20 06:12:32 -0500

    Created a typedNoop2.

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,3 +1,4 @@
 /* eslint-disable no-empty-function */
 export const noop = () => {};
 export const typedNoop = <T>() => (_: T) => {};
+export const typedNoop2 = <T1, T2>() => (_1: T1, _2: T2) => {};
